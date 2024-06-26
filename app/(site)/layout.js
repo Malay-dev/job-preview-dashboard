d18d146df6f5d4bcb16ab9afdea783e0a8e81bea
Malay Kumar <malaykumar2003@gmail.com>
2024-06-26 17:45:52 +0530

    Feat: App Init + Layout

---
+++ app/(site)/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavDash from "@/components/NavDash";
+
+const DashboardLayout = () => {
+  return (
+    <div>
+      <NavDash></NavDash>
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ app/(site)/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>glenlkwnegw</div>;
+};
+
+export default page;
