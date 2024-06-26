0dd48afc68ced01d8e2761d5556815bfd320f0b0
Malay Kumar <malaykumar2003@gmail.com>
2024-06-26 19:06:09 +0530

    Feat: NavBar completed

---
+++ components/Icons.jsx
@@ -0,0 +1,15 @@
+import {
+  Briefcase,
+  MessageSquare,
+  HandCoins,
+  Bell,
+  ChevronDown,
+} from "lucide-react";
+
+export const Icons = {
+  Briefcase: Briefcase,
+  MessageSquare: MessageSquare,
+  HandCoins: HandCoins,
+  Bell: Bell,
+  ChevronDown: ChevronDown,
+};
---
+++ components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import NavTab from "./NavTab";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { Icons } from "./Icons";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@nextui-org/react";
+
+const NavBar = () => {
+  return (
+    <div className="container flex items-center justify-between h-20 border-b">
+      <Link
+        href="/"
+        className={`${buttonVariants({ variant: "Secondary" })} bg-gray-100`}>
+        <p className="text-[#DC4A2D]">Logo</p>
+      </Link>
+      <div>
+        <NavTab></NavTab>
+      </div>
+      <div className="flex items-center gap-2">
+        <Badge content="" className="bg-[#DC4A2d]" size="sm" shape="circle">
+          <Icons.Bell></Icons.Bell>
+        </Badge>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Icons.ChevronDown></Icons.ChevronDown>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
