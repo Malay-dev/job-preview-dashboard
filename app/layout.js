f2543c10e7966322213366f65929eaa00dbf654f
Malay Kumar <malaykumar2003@gmail.com>
2024-06-26 17:45:41 +0530

    Init

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import { Inter } from "next/font/google";
+import NavBar from "@/components/NavBar";
+import { NextUIProvider } from "@nextui-org/system";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextUIProvider>
+          <NavBar></NavBar>
+          {children}
+        </NextUIProvider>
+      </body>
     </html>
   );
 }
