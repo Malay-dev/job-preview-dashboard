9923058b7aeb65028f874ed6b6630e461d8b5c50
Malay Kumar <malaykumar2003@gmail.com>
2024-06-27 19:57:51 +0530

    Fix: Nav Borders

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,24 +8,26 @@ import { Badge } from "@nextui-org/react";
 
 const NavBar = () => {
   return (
-    <div className="container flex items-center justify-between h-20 border-b">
-      <Link
-        href="/"
-        className={`${buttonVariants({ variant: "Secondary" })} bg-gray-100`}>
-        <p className="text-[#DC4A2D]">Logo</p>
-      </Link>
-      <div>
-        <NavTab></NavTab>
-      </div>
-      <div className="flex items-center gap-3">
-        <Badge content="" className="bg-[#DC4A2d]" size="sm" shape="circle">
-          <Icons.Bell></Icons.Bell>
-        </Badge>
-        <Avatar>
-          <AvatarImage src="/atlassian-logo.png" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-        <Icons.ChevronDown></Icons.ChevronDown>
+    <div className="border-b">
+      <div className="container flex items-center h-20 justify-between">
+        <Link
+          href="/"
+          className={`${buttonVariants({ variant: "Secondary" })} bg-gray-100`}>
+          <p className="text-[#DC4A2D]">Logo</p>
+        </Link>
+        <div>
+          <NavTab></NavTab>
+        </div>
+        <div className="flex items-center gap-3">
+          <Badge content="" className="bg-[#DC4A2d]" size="sm" shape="circle">
+            <Icons.Bell></Icons.Bell>
+          </Badge>
+          <Avatar>
+            <AvatarImage src="/atlassian-logo.png" />
+            <AvatarFallback>A</AvatarFallback>
+          </Avatar>
+          <Icons.ChevronDown></Icons.ChevronDown>
+        </div>
       </div>
     </div>
   );
--- components/NavDash.jsx
+++ components/NavDash.jsx
@@ -3,22 +3,24 @@ import React from "react";
 import { Tabs, Tab } from "@nextui-org/react";
 const NavDash = () => {
   return (
-    <div className="container h-10  border-b w-full">
-      <Tabs
-        color="primary"
-        variant="underlined"
-        aria-label="Tabs"
-        classNames={{
-          tabList: "gap-20 w-full relative rounded-none p-0 border-divider",
-          cursor: "w-full bg-[#DC4A2D]",
-          tab: "max-w-fit px-0 h-10",
-          tabContent: "group-data-[selected=true]:text-[#DC4A2D]",
-        }}>
-        <Tab key="job-preview" title="Job preview" />
-        <Tab key="applicants" title="Applicants" />
-        <Tab key="match" title="Match" />
-        <Tab key="messages" title="Messages" />
-      </Tabs>
+    <div className="h-10  border-b w-full">
+      <div className="container">
+        <Tabs
+          color="primary"
+          variant="underlined"
+          aria-label="Tabs"
+          classNames={{
+            tabList: "gap-20 w-full relative rounded-none p-0 border-divider",
+            cursor: "w-full bg-[#DC4A2D]",
+            tab: "max-w-fit px-0 h-10",
+            tabContent: "group-data-[selected=true]:text-[#DC4A2D]",
+          }}>
+          <Tab key="job-preview" title="Job preview" />
+          <Tab key="applicants" title="Applicants" />
+          <Tab key="match" title="Match" />
+          <Tab key="messages" title="Messages" />
+        </Tabs>
+      </div>
     </div>
   );
 };
