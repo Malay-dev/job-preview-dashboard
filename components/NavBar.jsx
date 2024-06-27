4026b33b0194433c44874acd582a67165414c568
Malay Kumar <malaykumar2003@gmail.com>
2024-06-27 23:09:55 +0530

    Fix: NavBar dimensions

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -9,7 +9,7 @@ import { Badge } from "@nextui-org/react";
 const NavBar = () => {
   return (
     <div className="border-b">
-      <div className="container flex items-center h-20 justify-between">
+      <div className="container flex items-center h-16 justify-between">
         <Link
           href="/"
           className={`${buttonVariants({ variant: "Secondary" })} bg-gray-100`}>
@@ -20,13 +20,13 @@ const NavBar = () => {
         </div>
         <div className="flex items-center gap-3">
           <Badge content="" className="bg-[#DC4A2d]" size="sm" shape="circle">
-            <Icons.Bell></Icons.Bell>
+            <Icons.Bell size={"20"}></Icons.Bell>
           </Badge>
           <Avatar>
             <AvatarImage src="/atlassian-logo.png" />
             <AvatarFallback>A</AvatarFallback>
           </Avatar>
-          <Icons.ChevronDown></Icons.ChevronDown>
+          <Icons.ChevronDown size={"20"}></Icons.ChevronDown>
         </div>
       </div>
     </div>
--- components/NavTab.jsx
+++ components/NavTab.jsx
@@ -6,20 +6,20 @@ import { Badge } from "@nextui-org/react";
 const NavTab = () => {
   return (
     <div>
-      <Tabs defaultValue="jobs" className="w-[500px]">
+      <Tabs defaultValue="jobs" className="w-[400px] text-sm">
         <TabsList className="grid w-full grid-cols-3 place-content-center rounded-3xl border">
           <TabsTrigger value="jobs" className="rounded-2xl flex gap-2">
-            <Icons.Briefcase></Icons.Briefcase>
+            <Icons.Briefcase size={"20"}></Icons.Briefcase>
             Jobs
           </TabsTrigger>
           <TabsTrigger value="messages" className="rounded-2xl flex gap-2">
             <Badge content="" className="bg-[#DC4A2d]" size="sm" shape="circle">
-              <Icons.MessageSquare></Icons.MessageSquare>
+              <Icons.MessageSquare size={"20"}></Icons.MessageSquare>
             </Badge>
             Messages
           </TabsTrigger>
           <TabsTrigger value="payments" className="rounded-2xl flex gap-2">
-            <Icons.HandCoins></Icons.HandCoins>
+            <Icons.HandCoins size={"20"}></Icons.HandCoins>
             Payments
           </TabsTrigger>
         </TabsList>
